cdc14d7d809e917b82c76ef7397b255749d7c4a5
Jens Låås <jens@laas.mine.nu>
2010-11-22 12:19:16 +0100

    import

---
+++ Makefile
@@ -0,0 +1,57 @@
+#%switch --prefix PREFIX
+#%switch --sysconfdir SYSCONFDIR
+#%switch --libdir LIBDIR
+#%switch --libexecdir LIBEXECDIR
+#%switch --localstatedir LOCALSTATEDIR
+#%switch --bindir BINDIR
+#%switch --mandir MANDIR
+#%ifnswitch --prefix /usr PREFIX
+#%ifnswitch --sysconfdir /etc SYSCONFDIR
+#%ifnswitch --libdir $(PREFIX)/lib LIBDIR
+#%ifnswitch --libexecdir $(PREFIX)/libexec LIBEXECDIR
+#%ifnswitch --localstatedir /var/lib LOCALSTATEDIR
+#%ifnswitch --bindir $(PREFIX)/bin BINDIR
+#%ifnswitch --mandir $(PREFIX)/share/man MANDIR
+#?V=`cat version.txt|cut -d ' ' -f 2`
+#?CC=./shpp.sh
+#?prg=derive
+#?export LIBEXECDIR
+#?export BINDIR
+#?export SYSCONFDIR
+#?export LOCALSTATEDIR
+#?%:	%.sh Makefile
+#?	VERSION=$(V) ./shpp.sh $<
+#?all:	$(prg)
+#?install:	$(prg)
+#?	mkdir -p $(DESTDIR)$(BINDIR)
+#?	cp -f $(prg) $(DESTDIR)$(BINDIR)
+#?install:	install
+#?clean:
+#?	rm -f $(prg)
+#?tarball:	clean
+#?	make-tarball.sh
+PREFIX= /usr
+SYSCONFDIR= /etc
+LIBDIR= $(PREFIX)/lib
+LIBEXECDIR= $(PREFIX)/libexec
+LOCALSTATEDIR= /var/lib
+BINDIR= $(PREFIX)/bin
+MANDIR= $(PREFIX)/share/man
+V=`cat version.txt|cut -d ' ' -f 2`
+CC=./shpp.sh
+prg=derive
+export LIBEXECDIR
+export BINDIR
+export SYSCONFDIR
+export LOCALSTATEDIR
+%:	%.sh Makefile
+	VERSION=$(V) ./shpp.sh $<
+all:	$(prg)
+install:	$(prg)
+	mkdir -p $(DESTDIR)$(BINDIR)
+	cp -f $(prg) $(DESTDIR)$(BINDIR)
+install:	install
+clean:
+	rm -f $(prg)
+tarball:	clean
+	make-tarball.sh
---
+++ derive.sh
@@ -0,0 +1,298 @@
+#!/bin/bash
+#
+# File: derive.sh
+# Implements: proto-derivat control
+#
+# Copyright: Jens Låås, UU 2010
+# Copyright license: According to GPL, see file COPYING in this directory.
+#
+
+VERSION=VERSION
+DATE=DATE
+
+function usage {
+    echo "derive $VERSION ($DATE)"
+    echo
+    echo "Invocations:"
+    echo " derive"
+    echo " derive fetch"
+    echo " derive diff"
+    echo " derive apply"
+    echo " derive <derivat> fetch"
+    echo " derive <derivat> diff"
+    echo " derive <derivat> apply"
+    echo " derive <derivat> init <proto-repository> [apply]"
+}
+
+function gitpath {
+    local P
+    P="$(pwd)"
+
+    while true; do
+	[ -d "$P/.git" ] && echo $P && return 0
+	P="$(dirname $P)"
+	[ "$P" = "/" ] && return 1
+    done
+}
+
+function fetch_files {
+    local F REPO
+    REPO=$1
+    
+    while read F; do
+	if [ ! -f "$F" ]; then
+	    mkdir -p "$(dirname $F)"
+	    cp "$REPO/$F" "$F"
+	fi
+    done
+}
+
+function diff_files {
+    local F REPO
+    REPO=$1
+    
+    while read F; do
+	if [ -f "$F" ]; then
+	    diff -u "$F" "$REPO/$F"
+	else
+	    echo "$F not yet fetched from repo."
+	fi
+    done
+}
+
+function apply_files {
+    local F REPO
+    REPO=$1
+    
+    while read F; do
+	mkdir -p "$(dirname $F)"
+	cp "$REPO/$F" "$F"
+    done
+}
+
+function derive_fetch {
+    DERIVAT="$1"
+    GITPATH="$(gitpath)"
+    
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+    
+    REPO=$(cat ".derivats/$DERIVAT"|head -n 1)
+    
+    cat $REPO/$DERIVAT|fetch_files "$REPO"
+}
+
+function derive_apply {
+    DERIVAT="$1"
+    GITPATH="$(gitpath)"
+    
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+    
+    REPO=$(cat ".derivats/$DERIVAT"|head -n 1)
+    
+    cat $REPO/$DERIVAT|apply_files "$REPO"
+}
+
+function derive_diff {
+    DERIVAT="$1"
+    GITPATH="$(gitpath)"
+    
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+    
+    REPO=$(cat ".derivats/$DERIVAT"|head -n 1)
+    
+    cat $REPO/$DERIVAT|diff_files "$REPO"
+}
+
+function derive_apply {
+    DERIVAT="$1"
+    GITPATH="$(gitpath)"
+    
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+    
+    REPO=$(cat ".derivats/$DERIVAT"|head -n 1)
+    
+    cat $REPO/$DERIVAT|apply_files "$REPO"
+}
+
+if [ "${1:0:1}" = '-' ]; then
+    usage
+    exit 0
+fi
+
+#
+# derive
+#
+if [ -z "$1" ]; then
+    GITPATH="$(gitpath)"
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+
+    if [ ! -d .derivats ]; then
+	echo "Cannot read '.derivats'." >&2
+	exit 2
+    fi
+
+    for DERIVAT in .derivats/*; do
+	basename $DERIVAT
+    done
+    exit 0
+fi
+
+#
+# derive fetch
+#
+if [ "$1" = fetch -a -z "$2" ]; then
+    GITPATH="$(gitpath)"
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+
+    if [ ! -d .derivats ]; then
+	echo "Cannot read '.derivats'." >&2
+	exit 2
+    fi
+
+    for DERIVAT in .derivats/*; do
+	derive_fetch $(basename $DERIVAT)
+    done
+
+    exit
+fi
+
+#
+# derive diff
+#
+if [ "$1" = diff -a -z "$2" ]; then
+    GITPATH="$(gitpath)"
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+
+    if [ ! -d .derivats ]; then
+	echo "Cannot read '.derivats'." >&2
+	exit 2
+    fi
+
+    for DERIVAT in .derivats/*; do
+	derive_diff $(basename $DERIVAT)
+    done
+
+    exit
+fi
+
+#
+# derive apply
+#
+if [ "$1" = apply -a -z "$2" ]; then
+    GITPATH="$(gitpath)"
+    if [ -z "$GITPATH" ]; then
+	echo "derive can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+
+    if [ ! -d .derivats ]; then
+	echo "Cannot read '.derivats'." >&2
+	exit 2
+    fi
+
+    for DERIVAT in .derivats/*; do
+	derive_apply $(basename $DERIVAT)
+    done
+
+    exit
+fi
+
+#
+# derive <derivat> fetch
+#
+if [ "$1" -a "$2" = fetch ]; then
+    derive_fetch "$1"
+    exit
+fi
+
+#
+# derive <derivat> diff
+#
+if [ "$1" -a "$2" = diff ]; then
+    derive_diff "$1"
+    exit
+fi
+
+#
+# derive <derivat> apply
+#
+if [ "$1" -a "$2" = apply ]; then
+    derive_apply "$1"
+    exit
+fi
+
+#
+# derive <derivat> init <proto-repository> [apply]
+#
+if [ "$1" -a "$2" = init -a "$3" ]; then
+    REPO="$3"
+    DERIVAT="$1"
+    GITPATH="$(gitpath)"
+    
+    if [ -z "$GITPATH" ]; then
+	echo "derive init can only be done in a git repository!" >&2
+	exit 1
+    fi
+    
+    cd $GITPATH
+    
+    mkdir -p .derivats
+    
+    if [ ! -f "$REPO/.proto-repository" ]; then
+	echo "$REPO is not a proto-repository!"
+	echo ".proto-repository missing."
+	exit 1
+    fi
+    [ -f "$REPO/$DERIVAT" ] || exit 1
+    [ -e ".derivats/$DERIVAT" ] && exit 1
+    
+    if [ "$4" = apply ]; then
+	echo $REPO > .derivats/$DERIVAT
+	echo $DERIVAT >> .derivats/$DERIVAT
+	
+	derive_fetch "$DERIVAT"
+    else
+	cat $REPO/$DERIVAT
+    fi
+    exit 0
+fi
+
+usage
+exit 1
